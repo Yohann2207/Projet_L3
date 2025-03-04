a70bda36e02c414f395fc516b5c7284474535f69
Yohann2207 <yoyofbi2a@gmail.com>
2025-03-04 11:00:49 +0100

    Merge branch 'master' of https://github.com/Yohann2207/Projet_L3.git

--- src/BDD/BDD.java
+++ src/BDD/BDD.java
@@ -13,7 +13,7 @@ public class BDD {
     }
     public static void ajouter_res() {
         //Requete SQL pour ajouter la ressource (paramètres variables selon notre future BDD) 
-        System.out.println("La ressource a ete ajoute avec succes"); 
+        System.out.println("La ressource a ete ajoute avec succes"); //test pull and commit
     }
 
     public static void supprimer_res(int id) {
