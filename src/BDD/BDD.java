324ab1ded5736980c5a1827549748a551a952e8d
Manon <>
2025-03-04 10:57:11 +0100

    test

--- src/BDD/BDD.java
+++ src/BDD/BDD.java
@@ -13,7 +13,7 @@ public class BDD {
     }
     public static void ajouter_res() {
         //Requete SQL pour ajouter la ressource (paramètres variables selon notre future BDD) 
-        System.out.println("La ressource a ete ajoute avec succes"); //commentaire piuyr test 
+        System.out.println("La ressource a ete ajoute avec succes"); //commentaire piuyr test dre
     }
 
     public static void supprimer_res(int id) {
